9599edfd9a178fcfa2af42db46a4b25a95568bbc
“guhyeon” <guhyeon77@gmail.com>
2020-07-11 15:47:51 +0900

    TodoList 설계

--- src/App.js
+++ src/App.js
@@ -1,19 +1,28 @@
 import React from 'react';
-import logo from './logo.svg';
 import './App.css';
-import test from './images/test.jpeg'
 import TodoItemInput from './Components/TodoItemInput';
 
 // 숙제. 아래 onPlusClick을 함수로 따로 빼서 만들어줘
 function App() {
   return (
     <div className="App">
-        <TodoItemInput onPlusClick={
+        <TodoItemInput 
+          onPlusClick={
           (value)=>{
             // console.log("InputPlusClick from TodoItemInput" + value)
             console.log(`InputPlusClick from TodoItemInput ${value}`)
           }
-        } ></TodoItemInput>
+        }></TodoItemInput>
+        {
+          // TodoList에서는 어떤 아이템이 완료됐는지, 제거해야하는지를 뱉어주는 이벤트를 속성으로 갖는다.
+        }
+        <TodoList 
+          onDeleteClick = {(item)=>{
+            console.log("App -> item", item)
+          }}
+          onCompleteClick = {(item)=>{
+            console.log("App -> item", item)
+          }}/>
     </div>
   );
 }
