3d33e5ad6a0fdea66d36eb97b7fa93a62e27de52
“guhyeon” <guhyeon77@gmail.com>
2020-07-11 16:33:01 +0900

    스테이트 끌어올리기 설명

--- src/App.js
+++ src/App.js
@@ -5,6 +5,12 @@ import TodoList from './Components/TodoList'
 
 // 숙제. 아래 onPlusClick을 함수로 따로 빼서 만들어줘
 function App() {
+  // TODO 리스트 아이템을 추가
+  // 이벤트시 리스트 아이템을 추가해주려면
+  // 1. 스테이트인 리스트데이터를 이곳에서 관리하거나
+  // 2. 리스트 컴포넌트에 추가한 아이템을 이벤트를 통해 넘겨주거나 하는 방법이있다.
+  // 1번은 스테이트 끌어올리기란 방법이고. 아주 중요하다
+  // 2번은 별로 권장하지 않는 방법.
   return (
     <div className="App">
         <TodoItemInput 
