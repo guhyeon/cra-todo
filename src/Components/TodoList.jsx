4d4f070541bd33f1afdaf6b12b982a0c630e8750
“guhyeon” <guhyeon77@gmail.com>
2020-07-11 16:48:14 +0900

    완료, 삭제 이벤트 반대로연결 수정

--- src/Components/TodoList.jsx
+++ src/Components/TodoList.jsx
@@ -13,7 +13,7 @@ const TodoList = ({list, onDeleteClick, onCompleteChange}) => {
                     // 리액트에서 배열값을 렌더링해줄때는 key 프로퍼티가 필요하다. 리액트에서는 변경된 값만 변경하는데, 키가없으면 뭐가 변경됐는지 어떻게알겠어?
                     // 그래서 고유한 키 값이 필요
                     return (
-                        <TodoListItem key={id} id = {id} text={text} onCompleteChange = {onDeleteClick} onDeleteClick = {onCompleteChange} ></TodoListItem>
+                        <TodoListItem key={id} id = {id} text={text} onCompleteChange = {onCompleteChange} onDeleteClick = {onDeleteClick} ></TodoListItem>
                     )
                 })
             }
