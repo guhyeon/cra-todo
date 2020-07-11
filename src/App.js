20d5f0b99b93b96f3edbdcf85f996dfc3177cdcd
DESKTOP-P78NMQQ\jk <m4a1z89@naver.com>
2020-07-11 13:58:22 +0900

    init

---
+++ src/App.js
@@ -0,0 +1,28 @@
+import React from 'react';
+import logo from './logo.svg';
+import mj from './mj.jpg';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <img src={logo} className="App-logo" alt="logo" />
+        <img src={mj} alt="logo" />
+        <p>
+          Edit <code>구현아 미안해 집에갈께</code> and save to reload.
+        </p>
+        <a
+          className="App-link"
+          href="https://reactjs.org"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          Learn React
+        </a>
+      </header>
+    </div>
+  );
+}
+
+export default App;
