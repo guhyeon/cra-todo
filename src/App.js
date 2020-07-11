a0a372e33ddeefeff59ba06099822abda11a3e4d
“guhyeon” <guhyeon77@gmail.com>
2020-07-11 16:35:35 +0900

    list 스테이트 끌어올리기

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-import React from 'react';
+import React,{useState, useEffect} from 'react';
 import './App.css';
 import TodoItemInput from './Components/TodoItemInput';
 import TodoList from './Components/TodoList'
@@ -11,6 +11,31 @@ function App() {
   // 2. 리스트 컴포넌트에 추가한 아이템을 이벤트를 통해 넘겨주거나 하는 방법이있다.
   // 1번은 스테이트 끌어올리기란 방법이고. 아주 중요하다
   // 2번은 별로 권장하지 않는 방법.
+  const [list, setList] = useState([]);
+
+  // 더미 리스트 생성 훅스. todoList가 생성되고나서 한 번만 생성한다. 두 번째 인자인 deps에 []. 
+  useEffect(()=>{
+    const dummyList = [
+        {id:0, text:"a", isComplete:false},
+        {id:1, text:"B", isComplete:true},
+        {id:2, text:"C", isComplete:false},
+        {id:3, text:"D", isComplete:true}
+    ]
+    setList(dummyList)
+  },[])
+
+
+  const handleCompleteChange = (itemId, isComplete)=>{
+      console.log("handleCompleteChange -> itemId, isComplete", itemId, isComplete)
+      // TODO 리스트에서 아이템 아이디에 맞는 항목의 완료값을 변경
+      
+  }
+
+  const handleDeleteClick = (itemId)=>{
+      console.log("handleDeleteClick -> itemId", itemId)
+      // TODO 리스트에서 해당 아이템 제거
+  }
+
   return (
     <div className="App">
         <TodoItemInput 
@@ -24,12 +49,9 @@ function App() {
           // TodoList에서는 어떤 아이템이 완료됐는지, 제거해야하는지를 뱉어주는 이벤트를 속성으로 갖는다.
         }
         <TodoList 
-          onDeleteClick = {(item)=>{
-            console.log("App -> onDeleteClick", item)
-          }}
-          onCompleteChange = {(item)=>{
-            console.log("App -> onCompleteChange", item)
-          }}/>
+          list={list}
+          onDeleteClick = {handleDeleteClick}
+          onCompleteChange = {handleCompleteChange}/>
     </div>
   );
 }
--- src/Components/TodoList.jsx
+++ src/Components/TodoList.jsx
@@ -1,31 +1,7 @@
-import React,{useState, useEffect} from 'react'
+import React from 'react'
 import TodoListItem from './TodoListItem';
 
-const TodoList = ({onDeleteClick, onCompleteChange}) => {
-    const [list, setList] = useState([]);
-
-    // 더미 리스트 생성 훅스. todoList가 생성되고나서 한 번만 생성한다. 두 번째 인자인 deps에 []. 
-    useEffect(()=>{
-        const dummyList = [
-            {id:0, text:"a", isComplete:false},
-            {id:1, text:"B", isComplete:true},
-            {id:2, text:"C", isComplete:false},
-            {id:3, text:"D", isComplete:true}
-        ]
-        setList(dummyList)
-    },[])
-
-
-    const handleCompleteChange = (itemId, isComplete)=>{
-        // TODO 리스트에서 아이템 아이디에 맞는 항목의 완료값을 변경
-        
-    }
-
-    const handleDeleteClick = (itemId)=>{
-        // TODO 리스트에서 해당 아이템 제거
-    }
-
-
+const TodoList = ({list, onDeleteClick, onCompleteChange}) => {
     return (
         <div>
             {
@@ -37,7 +13,7 @@ const TodoList = ({onDeleteClick, onCompleteChange}) => {
                     // 리액트에서 배열값을 렌더링해줄때는 key 프로퍼티가 필요하다. 리액트에서는 변경된 값만 변경하는데, 키가없으면 뭐가 변경됐는지 어떻게알겠어?
                     // 그래서 고유한 키 값이 필요
                     return (
-                        <TodoListItem key={id} id = {id} text={text} onCompleteChange = {handleCompleteChange} onDeleteClick = {handleDeleteClick} ></TodoListItem>
+                        <TodoListItem key={id} id = {id} text={text} onCompleteChange = {onDeleteClick} onDeleteClick = {onCompleteChange} ></TodoListItem>
                     )
                 })
             }
